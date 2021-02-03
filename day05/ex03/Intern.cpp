e6c173d1d0ac45aef5d29da50c09d65dd32ccd2c
Harthann <nca.eyraud@gmail.com>
2021-02-03 15:21:12 +0100

    removing comment on inter class

--- day05/ex03/Intern.cpp
+++ day05/ex03/Intern.cpp
@@ -1,8 +1,5 @@
 #include "Intern.hpp"
 
-//typedef Form* (*createForm)(std::string const&);
-//std::map<std::string, createForm> forms_map;
-
 Intern::Intern()
 {
 	this->key[0] = "robotomy request";
@@ -11,14 +8,10 @@ Intern::Intern()
 	this->value[0] = Intern::createRRF;
 	this->value[1] = Intern::createSCF;
 	this->value[2] = Intern::createPPF;
-//	forms_map.insert(std::make_pair("robotomy request", Intern::createRRF));
-//	forms_map.insert(std::make_pair("shrubbery creation", Intern::createSCF));
-//	forms_map.insert(std::make_pair("presidential pardon", Intern::createPPF));
 }
 
 Form* Intern::makeForm(std::string const& form, std::string const& target)
 {
-//	createForm f = forms_map[form];
 	for (int i = 0; i < 3; i++)
 	{
 		if (!form.compare(this->key[i]))
@@ -27,14 +20,8 @@ Form* Intern::makeForm(std::string const& form, std::string const& target)
 			return (this->value[i](target));
 		}
 	}
-//	if (!*f)
-//	{
 		std::cout << "Intern failed to create form he will be obviously fire\n";
 		return (NULL);
-//	}
-//	Form *tmp = (*f)(target);
-//	std::cout << "Intern creates " << tmp->getName() << std::endl;
-//	return (tmp);
 }
 
 Form* Intern::createRRF(std::string const& target)
