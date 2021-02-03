567c04fa2573da74436568efca19756a4e88eabe
Harthann <nca.eyraud@gmail.com>
2021-02-03 15:20:27 +0100

    Upgrading main in ex03

--- day05/ex03/main.cpp
+++ day05/ex03/main.cpp
@@ -12,28 +12,38 @@ int	main(void)
 		Bureaucrat eric("Eric", 30);
 		Bureaucrat sam("Sam", 1);
 		Intern *joe = new Intern();
-		
-		Form *lowForm = joe->makeForm("shrubbery creation", "home");
+
+		Form *firstForm = joe->makeForm("shrubbery creation", "home");
 		Form *secondForm = joe->makeForm("robotomy request", "Director");
 		Form *thirdForm = joe->makeForm("presidential pardon", "Eric");
+		std::cout << "===== BOB WITH FIRST FORM =====\n";
 		try {
-			bob.signForm(*lowForm);
-			bob.executeForm(*lowForm);
+			bob.signForm(*firstForm);
+			bob.executeForm(*firstForm);
+		}
+		catch (std::exception& e) {
+			std::cout << e.what() << std::endl; }
+		std::cout << "===== ERIC WITH SECOND FORM =====\n";
+		try {
 			eric.signForm(*secondForm);
 			eric.executeForm(*secondForm);
+		}
+		catch (std::exception& e) {
+			std::cout << e.what() << std::endl; }
+		std::cout << "===== SAM WITH THIRD FORM =====\n";
+		try {
 			sam.signForm(*thirdForm);
 			sam.executeForm(*thirdForm);
 		}
 		catch (std::exception& e) {
-			std::cout << e.what() << std::endl;
-		}
-		delete lowForm;
+			std::cout << e.what() << std::endl; }
+		delete firstForm;
 		delete secondForm;
 		delete thirdForm;	
 		delete joe;
+
 	}
 	catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-	}
+		std::cout << e.what() << std::endl; }
 	return (0);
 }
